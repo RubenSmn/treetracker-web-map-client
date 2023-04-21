0764f1a4c1f9da2b95b2a6b6eb2328cdd5bb07a1
RubenSmn <saaman.ruben@gmail.com>
2023-04-21 09:54:18 +0200

    chore: remove getting config from api

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,18 +8,18 @@ import { useRouter } from 'next/router';
 import Script from 'next/script';
 import { userAgentFromString } from 'next/server';
 import React from 'react';
+import Layout from 'components/Layout';
+import LayoutDashboard from 'components/LayoutDashboard';
+import LayoutEmbed from 'components/LayoutEmbed';
+import LayoutMobile from 'components/LayoutMobile';
+import LayoutMobileB from 'components/LayoutMobileB';
+import LayoutMobileC from 'components/LayoutMobileC';
+import { DrawerProvider } from 'context/DrawerContext';
+import { ConfigProvider, defaultConfig } from 'context/configContext';
+import { CustomThemeProvider } from 'context/themeContext';
+import { useLocalStorage, useEmbed } from 'hooks/globalHooks';
+import { MapContextProvider } from 'mapContext';
 import packageJson from '../../package.json';
-import Layout from '../components/Layout';
-import LayoutDashboard from '../components/LayoutDashboard';
-import LayoutEmbed from '../components/LayoutEmbed';
-import LayoutMobile from '../components/LayoutMobile';
-import LayoutMobileB from '../components/LayoutMobileB';
-import LayoutMobileC from '../components/LayoutMobileC';
-import { DrawerProvider } from '../context/DrawerContext';
-import { ConfigProvider, defaultConfig } from '../context/configContext';
-import { CustomThemeProvider } from '../context/themeContext';
-import { useLocalStorage, useEmbed } from '../hooks/globalHooks';
-import { MapContextProvider } from '../mapContext';
 
 log.warn(`Web Map Client version ${packageJson.version}`);
 
@@ -214,19 +214,7 @@ TreetrackerApp.getInitialProps = async (context) => {
 
   const device = userAgentFromString(userAgent)?.device.type || 'desktop';
 
-  let config = defaultConfig;
-  if (!process.env.NEXT_PUBLIC_SERVER_CONFIG_DISABLED) {
-    const mapConfigRequest = await fetch(
-      // TODO: use the ENV var, currently results in a bug with the theme editor
-      // `${process.env.NEXT_PUBLIC_CONFIG_API}/config`,
-      `https://dev-k8s.treetracker.org/map_config/config`,
-    );
-    // Temp solution since the backend does not have full support for the config
-    const configData = await mapConfigRequest.json();
-    config =
-      configData.find((item) => item.name === 'testing-config')?.data ||
-      defaultConfig;
-  }
+  const config = defaultConfig;
 
   return {
     props,
